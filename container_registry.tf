85895f504d19a1ba115c37130a84500349e46110
Kishore Kumar Behera <kishore.behera2010@gmail.com>
2023-05-26 09:26:17 +0530

    added az setup for runner

--- container_registry.tf
+++ container_registry.tf
@@ -1,8 +1,7 @@
-module "acr" {
-  source              = "github.com/terraform-azurerm-modules/terraform-azurerm-container-registry"
-  acr_name            = var.acr_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   sku                 = var.acr_sku
   admin_enabled       = var.admin_enabled
 }
